c55f9c301da437943e99b38e7b88ae29a7d404e3
Robin Lamarche-Perrin <Robin.Lamarche-Perrin@mis.mpg.de>
2018-12-10 15:44:58 +0100

    Corrected bug in url parameters

--- js/target.js
+++ js/target.js
@@ -1,7 +1,7 @@
 function getTab () {
 	var url = new URL (window.location.href);
 	var tab = url.searchParams.get ("t");
-	window.frames['right'].location.href = 'html/' + tab + '.html';
+	if (url.searchParams.has ("t")) { window.frames['right'].location.href = 'html/' + tab + '.html'; }
 }
 
 if (window.addEventListener) { window.addEventListener("load", getTab, false); }
