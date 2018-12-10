0aa9a6683ca15458c8e6de56deca4cedd01b244b
Robin Lamarche-Perrin <Robin.Lamarche-Perrin@mis.mpg.de>
2018-12-10 15:38:41 +0100

    Making access to tabs (through url parameters) possible

---
+++ js/target.js
@@ -0,0 +1,9 @@
+function getTab () {
+	var url = new URL (window.location.href);
+	var tab = url.searchParams.get ("t");
+	window.frames['right'].location.href = 'html/' + tab + '.html';
+}
+
+if (window.addEventListener) { window.addEventListener("load", getTab, false); }
+else if (window.attachEvent) { window.attachEvent("onload", getTab); }
+else { window.onload = getTab; }
